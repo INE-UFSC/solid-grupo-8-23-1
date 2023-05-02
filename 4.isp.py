b0381276efd647eed45cc2ad0188de548e2d2b14
Rafael Corrêa Bitencourt <rafael2004.correa@gmail.com>
2023-05-02 11:33:52 -0300

    Update 4.isp.py

--- 4.isp.py
+++ 4.isp.py
@@ -8,36 +8,13 @@ class IJanela(ABC):
     def fechar(self):
         raise NotImplementedError
 
+    def minimizar(self):
+        raise NotImplementedError
+
 class FixedJanela(IJanela):
     def mostrar_menu(self):
-        raise NotImplementedError
-        
-    def fechar(self):
         raise NotImplementedError
 
 class MenulessJanela(IJanela):
     def maximizar(self):
         raise NotImplementedError
-    
-    def minimizar(self):
-        raise NotImplementedError
-        
-    def fechar(self):
-        raise NotImplementedError
-
-class JanelaTamanhoFixo(FixedJanela):
-    def mostrar_menu(self):
-        pass
-    
-    def fechar(self):
-        pass
-
-class JanelaSemMenu(MenulessJanela):
-    def maximizar(self):
-        pass
-
-    def minimizar(self):
-        pass
-    
-    def mostrar_menu(self):
-        pass
